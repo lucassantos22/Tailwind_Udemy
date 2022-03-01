24276cff984ab56c820ac4f712bbf708de53c3cd
Lucas Santos <lufigueredo@uolinc.com>
2022-03-01 20:13:28 -0300

    chore: curso

---
+++ 07_ALINHAMENTO/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      'tablet': '640px'
+    },
+    extend: {
+      colors: {
+        'new-black': 'blue'
+      },
+      spacing: {
+        '50': '20rem'
+      }
+    }
+  },
+  plugins: [],
+}
