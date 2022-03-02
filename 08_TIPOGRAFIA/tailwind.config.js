88916302f9c687340dd5033e1ebce16b953f337d
Lucas Santos <lufigueredo@uolinc.com>
2022-03-02 01:49:33 -0300

    chore: curso

---
+++ 08_TIPOGRAFIA/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'arial': 'Arial'
+      },
+      fontSize: {
+        '12xl': '8rem'
+      }
+    }
+  },
+  plugins: [],
+}
